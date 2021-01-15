163d3ce6a1f3b8632ebb538b50e0fdd9e070d8be
wang10998588 <wang10998588@126.com>
2021-01-15 12:59:52 +0800

    日常提交

--- games101_01/src/rasterizer.rs
+++ games101_01/src/rasterizer.rs
@@ -1,16 +1,36 @@
-use std::iter::Map;
+use crate::triangle::Triangle;
+use std::collections::HashMap;
+use cgmath::Zero;
+
 type Vector3i = cgmath::Vector3<i32>;
 type Vector3f = cgmath::Vector3<f32>;
 type Vector2f = cgmath::Vector2<f32>;
 type Vector4f = cgmath::Vector4<f32>;
 type Matrix4f = cgmath::Matrix4<f32>;
 
+pub enum Buffers{
+    Color = 1,
+    Depth = 2
+}
+pub enum Primitive{
+    Line,
+    Triangle
+}
+
+pub struct pos_buf_id{
+    pub pos_id:i32
+}
+
+pub struct ind_buf_id{
+    pub ind_id:i32
+}
+
 pub struct Rasterizer{
     model:Matrix4f,
     view:Matrix4f,
     projection:Matrix4f,
-    pos_buf:Map<i32,Vec<Vector3f>>,
-    ind_buf:Map<i32,Vec<Vector3i>>,
+    pos_buf:HashMap<i32,Vec<Vector3f>>,
+    ind_buf:HashMap<i32,Vec<Vector3i>>,
     frame_buf:Vec<Vector3f>,
     depth_buf:Vec<f32>,
     width:i32,
@@ -20,8 +40,76 @@ pub struct Rasterizer{
 
 impl Rasterizer{
     pub fn new(w:i32,h:i32)->Rasterizer{
+        Rasterizer{
+            model:Matrix4f::from([0f32;4]),
+            view:Matrix4f::from([0f32;4]),
+            projection:Matrix4f::from([0f32;4]),
+            pos_buf:HashMap::new(),
+            ind_buf:HashMap::new(),
+            frame_buf:Vec::new(),
+            depth_buf:Vec::new(),
+            width:w,
+            height:h,
+            next_id:0
+        }
+    }
+
+    pub fn load_positions(&mut self,positions:Vec<Vector3f>)->pos_buf_id{
+        let id = self.get_next_id();
+        self.pos_buf.insert(id,positions);
+        pos_buf_id{pos_id:id}
+    }
+
+    pub fn load_indices(&mut self,indices:Vec<Vector3i>)->ind_buf_id{
+        let id = self.get_next_id();
+        self.ind_buf.insert(id,indices);
+        ind_buf_id{ind_id:id}
+    }
+
+    pub fn frame_buffer(&self)->&Vec<Vector3f>{
+        &self.frame_buf
+    }
+    pub fn set_model(&mut self,m:Matrix4f){self.model = m;}
+    pub fn set_view(&mut self,v:Matrix4f){self.view = v;}
+    pub fn set_projection(&mut self,p:Matrix4f){self.projection = p;}
+
+    pub fn clear(&mut self,buff:Buffers){
+        match buff {
+            Buffers::Color=>{
+                for i in 0..self.frame_buf.len() {
+                    self.frame_buf[i].set_zero();
+                }
+            },
+            Buffers::Depth=>{
+
+            }
+        }
+    }
+
+    pub fn set_pixel(&mut self,point:&Vector3i,color:Vector3f){
+        if point.x < 0 || point.x >= self.width
+            || point.y<0||point.y>=self.height{
+            return;
+        }
+        let ind = (self.height-1-point.y)*self.width + point.x;
+        self.frame_buf[ind as usize] = color;
+    }
+
+    fn draw_line(&self,begin:Vector3f,end:Vector3f){
 
     }
 
+    fn rasterize_wireframe(&self,t:&Triangle){
 
+    }
+
+    fn get_index(&self,x:i32,y:i32)->i32{
+        (self.height-y)*self.width+x
+    }
+
+    fn get_next_id(&mut self)->i32{
+        let ret = self.next_id;
+        self.next_id+=1;
+        ret
+    }
 }
--- games101_01/src/triangle.rs
+++ games101_01/src/triangle.rs
@@ -31,22 +31,22 @@ impl Triangle{
     }
 
     pub fn set_vertex(&mut self, ind:i32, ver:Vector3f){
-        self.v[ind] = ver;
+        self.v[ind as usize] = ver;
     }
 
     pub fn set_normal(&mut self, ind:i32, n:Vector3f){
-        self.normal[ind] = n;
+        self.normal[ind as usize] = n;
     }
 
     pub fn set_color(&mut self, ind:i32, r:f32, g:f32, b:f32){
         if r<0.0||r>255.0||g<0.0||g>255.0||b<0.0||b>255.0{
             panic!("Invalid color values");
         }
-        self.color[ind] = Vector3f::new(r/255f32,g/255f32,b/255f32);
+        self.color[ind as usize] = Vector3f::new(r/255f32,g/255f32,b/255f32);
     }
 
     pub fn set_tex_coord(&mut self, ind:i32, s:f32, t:f32){
-        self.tex_coords[ind] = Vector2f::new(s,t);
+        self.tex_coords[ind as usize] = Vector2f::new(s,t);
     }
 
     pub fn to_vector4(&self) ->[Vector4f;3]{
