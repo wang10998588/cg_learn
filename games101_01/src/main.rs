a44de6f9f7a5a0f08b7c0ece07ba0970c3870d12
wang10998588 <wang10998588@126.com>
2021-01-28 22:22:15 +0800

    Use sdl2 to replace opencv

--- games101_01/src/main.rs
+++ games101_01/src/main.rs
@@ -1,17 +1,20 @@
 mod rasterizer;
 mod triangle;
+use crate::rasterizer::{Rasterizer, Buffers, Primitive};
 
-use core::ffi::c_void;
-use opencv::{highgui, videoio};
 use cgmath::{Vector3, Matrix4, SquareMatrix};
 use cgmath::Matrix3;
-use crate::rasterizer::{Rasterizer, Buffers, Primitive};
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+/*use core::ffi::c_void;
+use opencv::{highgui, videoio};
 use opencv::core::{Size, CV_32FC3, Mat, MatTraitManual, Mat_AUTO_STEP, CV_8UC3, MatTrait};
 use opencv::videoio::VideoCaptureTrait;
-use opencv::highgui::imshow;
+use opencv::highgui::imshow;*/
 
 
-fn run() -> opencv::Result<()> {
+/*fn run() -> opencv::Result<()> {
     let window = "video capture";
     highgui::named_window(window, 1)?;
     #[cfg(feature = "opencv-32")]
@@ -34,7 +37,7 @@ fn run() -> opencv::Result<()> {
         }
     }
     Ok(())
-}
+}*/
 
 fn get_view_matrix(eye_pos:&Vector3<f32>)->Matrix4<f32>{
     let mut view = Matrix4::identity();
@@ -81,7 +84,7 @@ fn main() {
 
     }
 
-    let mut r = Rasterizer::new(700,700);
+    let mut r = Rasterizer::new(720,720);
     let eye_pos = Vector3::new(0f32,0f32,5f32);
 
     let pos = vec![Vector3::new(2f32,0f32,-2f32)
@@ -93,7 +96,22 @@ fn main() {
     let mut key = 0;
     let mut frame_count = 0;
 
-    while key != 27 {
+    //sdl2
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("run run",720,720)
+        .position_centered()
+        .vulkan()
+        .build()
+        .map_err(|e|e.to_string()).unwrap();
+    let mut canvas = window.into_canvas().build().map_err(|e|e.to_string()).unwrap();
+    let texture_creator = canvas.texture_creator();
+    let mut texture=texture_creator.create_texture_streaming(PixelFormatEnum::RGB24,720,720)
+        .map_err(|e|e.to_string()).unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: while key != 27 {
         r.clear(Buffers::Color);
         r.clear(Buffers::Depth);
         r.set_model(get_model_matrix(angle));
@@ -102,22 +120,53 @@ fn main() {
 
         r.draw(&pos_id,&ind_id,Primitive::Triangle);
 
+        texture.with_lock(None,|buffer,pitch|{
+            let frame_buf = r.frame_buffer();
+           for y in 0..720{
+               let row_offset = y*pitch;
+               let frame_row_offset = y*720;
+               for x in 0..720{
+                   let offset = row_offset + x*3;
+                   let frame_color = frame_buf[frame_row_offset+x];
+                   buffer[offset] = frame_color.x as u8;
+                   buffer[offset + 1] = frame_color.y as u8;
+                   buffer[offset + 2] = frame_color.z as u8;
+               }
+           }
+        });
+
+        canvas.clear();
+        canvas.copy(&texture,None,None);
+        canvas.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit{..}
+                    |Event::KeyDown {keycode:Some(Keycode::Escape),..}=>{
+                    break 'running
+                },
+                _ =>{}
+            }
+        }
+        /*
         let mut image;
         //let mut oimage:Mat = Mat::default().unwrap();
         unsafe {
-            image = Mat::new_rows_cols_with_data(700, 700, CV_32FC3, r.frame_buffer().as_mut_ptr() as *mut c_void, Mat_AUTO_STEP).unwrap();
+            image = Mat::new_rows_cols_with_data(720, 720, CV_32FC3, r.frame_buffer().as_mut_ptr() as *mut c_void, Mat_AUTO_STEP).unwrap();
         }
 
         //image.convert_to(&mut oimage,CV_8UC3,0.09,0.0);
 
         highgui::imshow("run run",&mut image);
 
+
+        key = highgui::wait_key(10).unwrap();*/
+
+
         frame_count+=1;
         println!("frame count: {}",frame_count);
-
-        key = highgui::wait_key(10).unwrap();
-
     }
+
     /*let mut inputArg = String::new();
     std::io::stdin().read_line(&mut inputArg);*/
 
